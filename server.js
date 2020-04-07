c67625daa254eee2ea049966702642192b5ba579
MuhammadBilal <muhammadbilal@Bilal-MacBook-Pro.local>
2020-04-07 23:14:44 +0500

    aboutus compress mores

--- server.js
+++ server.js
@@ -103,14 +103,8 @@ app.post('/api/forms', (req, res) => {
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
     // Set static folder
-    // app.use(express.static('client/build', { maxAge: 2592000000 }));
-    app.use(express.static('client/build', {
-        maxAge: 86400000,
-        setHeaders: function(res, path) {
-            res.setHeader("Expires", new Date(Date.now() + 2592000000*30).toUTCString());
-          }
-    }))
-
+    // app.use(expressStaticGzip('client/build'));
+    app.use(express.static('client/build', { maxAge: 2592000000 }));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
